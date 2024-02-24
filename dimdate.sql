79a88aa3b43dddbc864b1e5672421f94fc6bddbc
Hilario_Lumbreras <59767003+hilario-lumbreras@users.noreply.github.com>
2024-02-24 08:07:57 -0600

    Update dimdate.sql

--- dimdate.sql
+++ dimdate.sql
@@ -1,45 +1,45 @@
 IF NOT EXISTS (SELECT * FROM sys.external_file_formats WHERE name = 'SynapseDelimitedTextFormat') 
-	CREATE EXTERNAL FILE FORMAT [SynapseDelimitedTextFormat] 
-	WITH ( FORMAT_TYPE = DELIMITEDTEXT ,
-	       FORMAT_OPTIONS (
-			 FIELD_TERMINATOR = ',',
-			 USE_TYPE_DEFAULT = FALSE
-			))
+BEGIN
+    CREATE EXTERNAL FILE FORMAT [SynapseDelimitedTextFormat] 
+    WITH (
+        FORMAT_TYPE = DELIMITEDTEXT ,
+        FORMAT_OPTIONS (
+            FIELD_TERMINATOR = ',',
+            USE_TYPE_DEFAULT = FALSE
+        )
+    );
+END
 GO
 
 IF NOT EXISTS (SELECT * FROM sys.external_data_sources WHERE name = 'udacitydemo2hqxb_udacitydemo2hqxb_dfs_core_windows_net') 
-	CREATE EXTERNAL DATA SOURCE [udacitydemo2hqxb_udacitydemo2hqxb_dfs_core_windows_net] 
-	WITH (
-		LOCATION = 'abfss://udacitydemo2hqxb@udacitydemo2hqxb.dfs.core.windows.net' 
-	)
+BEGIN
+    CREATE EXTERNAL DATA SOURCE [udacitydemo2hqxb_udacitydemo2hqxb_dfs_core_windows_net] 
+    WITH (
+        LOCATION = 'abfss://udacitydemo2hqxb@udacitydemo2hqxb.dfs.core.windows.net' 
+    );
+END
 GO
 
 CREATE EXTERNAL TABLE dbo.DimDate (
-	[DateKey] datetime,
-	[Date] nvarchar(4000),
-	[Year] nvarchar(4000),
-	[Quarter] nvarchar(4000),
-	[Month] nvarchar(4000),
-	[Day] nvarchar(4000),
-	[DayOfWeek] nvarchar(4000),
-	[Weekend] nvarchar(4000)
-	)
+    [DateKey] datetime,
+    [Date] nvarchar(4000),
+    [Year] nvarchar(4000),
+    [Quarter] nvarchar(4000),
+    [Month] nvarchar(4000),
+    [Day] nvarchar(4000),
+    [DayOfWeek] nvarchar(4000),
+    [Weekend] nvarchar(4000)
+)
 GO
 
-
-
-INSERT INTO dimDate (date_key, date, year, quarter, month, day, week, is_weekend)
-SELECT DISTINCT(TO_CHAR(payment_date :: DATE, 'yyyyMMDD')::integer) AS date_key,
-       date(payment_date)                                           AS date,
-       EXTRACT(year FROM payment_date)                              AS year,
-       EXTRACT(quarter FROM payment_date)                           AS quarter,
-       EXTRACT(month FROM payment_date)                             AS month,
-       EXTRACT(day FROM payment_date)                               AS day,
-       EXTRACT(week FROM payment_date)                              AS week,
-       CASE WHEN EXTRACT(ISODOW FROM payment_date) IN (6, 7) THEN true ELSE false END AS is_weekend
-FROM dbo.StagingPayment (
-	[payment_id] bigint,
-	[date] datetime2(0),
-	[amount] float,
-	[rider_id] bigint
-	)
+INSERT INTO dbo.DimDate (DateKey, [Date], [Year], [Quarter], [Month], [Day], [DayOfWeek], [Weekend])
+SELECT DISTINCT
+    TO_CHAR(payment_date :: DATE, 'yyyyMMdd')::integer AS DateKey,
+    TO_CHAR(payment_date :: DATE, 'yyyy-MM-dd') AS [Date],
+    EXTRACT(year FROM payment_date) AS [Year],
+    EXTRACT(quarter FROM payment_date) AS [Quarter],
+    EXTRACT(month FROM payment_date) AS [Month],
+    EXTRACT(day FROM payment_date) AS [Day],
+    TO_CHAR(payment_date, 'D') AS [DayOfWeek],
+    CASE WHEN TO_CHAR(payment_date, 'D') IN ('6', '7') THEN 'true' ELSE 'false' END AS [Weekend]
+FROM dbo.StagingPayment;
