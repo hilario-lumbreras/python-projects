5b75f115cbf33dce112056994fc946d2e936d216
Hilario_Lumbreras <59767003+hilario-lumbreras@users.noreply.github.com>
2024-02-27 17:20:39 -0600

    Update feb_2027.py

--- feb_2027.py
+++ feb_2027.py
@@ -1,4 +1,11 @@
-original_name = file_name_equal_df.iloc[0][0]
-cleaned_name = 	file_name_equal_df.iloc[0][1]	
-json_name = file_name_equal_df.iloc[0][2]
-csv_name = file_name_equal_df.iloc[0][3]
+def send_request(latitude, longitude):
+    wkid = 4326
+    units = 'Meters'
+    includeDate = False
+    url = f'https://epqs.nationalmap.gov/v1/json?x={longitude}&y={latitude}&wkid={wkid}&units={units}&includeDate={includeDate}'
+    response = urllib.request.urlopen(url)
+    response_string = response.read().decode('utf-8')
+    response_dict = json.loads('{' + response_string.split('{', 1)[1])
+    value = response_dict['value']
+    value = float(value)
+    return value
